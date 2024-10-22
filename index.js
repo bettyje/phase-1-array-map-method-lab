1260a898c7cdce9e4817fd9c67873dd4486a8ef4
bettyje <bettyjelagat156@gmail.com>
2024-10-22 23:44:08 +0300

    Add titleCased function to format tutorial titles

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+    return tutorials.map(function(tutorial) {
+        return tutorial
+            .split(' ') // Split the
+            .map(function(word) {
+                // Capitalize the first letter and concatenate with the rest of the word
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            })
+            .join(' '); // Join the words back into a single string
+    });
 }
